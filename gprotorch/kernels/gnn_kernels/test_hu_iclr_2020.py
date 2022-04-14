95f6f9d8cd6075e4718d44beeb3c64b39d000bbd
Leo Klarner <leo.klarner@bnc.ox.ac.uk>
2022-04-14 15:27:59 +0100

    Formatting and docstrings.

--- gprotorch/kernels/gnn_kernels/test_hu_iclr_2020.py
+++ gprotorch/kernels/gnn_kernels/test_hu_iclr_2020.py
@@ -7,46 +7,46 @@ Author: Leo Klarner (https://github.com/leojklarner), April 2022
 """
 
 import os
-import numpy as np
 
+import numpy as np
 import torch
 import torch.nn.functional as F
-
+from rdkit import Chem
+from torch_geometric.data import Data
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import add_self_loops
-from torch_geometric.data import Data
 from torch_scatter import scatter_add
-
-from rdkit import Chem
 
 # define the allowable node and edge labels as used in Hu et al.
 allowable_features = {
-    'possible_atomic_num_list': list(range(1, 119)),
-    'possible_chirality_list': [
+    "possible_atomic_num_list": list(range(1, 119)),
+    "possible_chirality_list": [
         Chem.rdchem.ChiralType.CHI_UNSPECIFIED,
         Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW,
         Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CCW,
-        Chem.rdchem.ChiralType.CHI_OTHER
+        Chem.rdchem.ChiralType.CHI_OTHER,
     ],
-    'possible_bonds': [
+    "possible_bonds": [
         Chem.rdchem.BondType.SINGLE,
         Chem.rdchem.BondType.DOUBLE,
         Chem.rdchem.BondType.TRIPLE,
-        Chem.rdchem.BondType.AROMATIC
+        Chem.rdchem.BondType.AROMATIC,
     ],
     # (E)/(Z) double bond stereo information
-    'possible_bond_dirs': [
+    "possible_bond_dirs": [
         Chem.rdchem.BondDir.NONE,
         Chem.rdchem.BondDir.ENDUPRIGHT,
-        Chem.rdchem.BondDir.ENDDOWNRIGHT
-    ]
+        Chem.rdchem.BondDir.ENDDOWNRIGHT,
+    ],
 }
 
 # define additional tokens for label masking
 num_atom_type = 120  # including the extra mask tokens
 num_chirality_tag = 3
 
-num_bond_type = 6  # including aromatic and self-loop edge, and extra masked tokens
+num_bond_type = (
+    6  # including aromatic and self-loop edge, and extra masked tokens
+)
 num_bond_direction = 3
 self_loop_token = 4  # bond type for self-loop edge
 masked_bond_token = 5  # bond type for masked edges
@@ -68,10 +68,16 @@ def mol_to_pyg(mol):
     # derive atom features: atomic number + chirality tag
     atom_features = []
     for atom in mol.GetAtoms():
-        atom_features.append([
-            allowable_features['possible_atomic_num_list'].index(atom.GetAtomicNum()),
-            allowable_features['possible_chirality_list'].index(atom.GetChiralTag())
-        ])
+        atom_features.append(
+            [
+                allowable_features["possible_atomic_num_list"].index(
+                    atom.GetAtomicNum()
+                ),
+                allowable_features["possible_chirality_list"].index(
+                    atom.GetChiralTag()
+                ),
+            ]
+        )
     atom_features = torch.tensor(np.array(atom_features), dtype=torch.long)
 
     # derive bond features: bond type + bond direction
@@ -86,8 +92,10 @@ def mol_to_pyg(mol):
         edge_index.append((j, i))
 
         # calculate edge features and append them to feature list
-        edge_feature = [allowable_features['possible_bonds'].index(bond.GetBondType()),
-                        allowable_features['possible_bond_dirs'].index(bond.GetBondDir())]
+        edge_feature = [
+            allowable_features["possible_bonds"].index(bond.GetBondType()),
+            allowable_features["possible_bond_dirs"].index(bond.GetBondDir()),
+        ]
         edge_attr.append(edge_feature)
         edge_attr.append(edge_feature)
 
@@ -107,12 +115,18 @@ class GINConv(MessagePassing):
     """
 
     def __init__(self, emb_dim, aggr="add"):
+        """
+        Initialise GIN convolutional layer.
+        Args:
+            emb_dim: latent node embedding dimension
+            aggr: aggregation procedure
+        """
         super(GINConv, self).__init__()
 
         self.mlp = torch.nn.Sequential(
             torch.nn.Linear(emb_dim, 2 * emb_dim),
             torch.nn.ReLU(),
-            torch.nn.Linear(2 * emb_dim, emb_dim)
+            torch.nn.Linear(2 * emb_dim, emb_dim),
         )
 
         self.edge_embedding1 = torch.nn.Embedding(num_bond_type, emb_dim)
@@ -123,6 +137,17 @@ class GINConv(MessagePassing):
         self.aggr = aggr
 
     def forward(self, x, edge_index, edge_attr):
+        """
+        Message passing and aggregation function
+        of the adapted GIN convolutional layer.
+        Args:
+            x: node features
+            edge_index: adjacency list
+            edge_attr: edge features
+
+        Returns: transformed and aggregated node embeddings
+
+        """
 
         # add self loops to edge index
         edge_index, _ = add_self_loops(edge_index, num_nodes=x.size(0))
@@ -130,11 +155,15 @@ class GINConv(MessagePassing):
         # update edge attributes to represent self-loop edges
         self_loop_attr = torch.zeros(x.size(0), 2)
         self_loop_attr[:, 0] = self_loop_token
-        self_loop_attr = self_loop_attr.to(edge_attr.device).to(edge_attr.dtype)
+        self_loop_attr = self_loop_attr.to(edge_attr.device).to(
+            edge_attr.dtype
+        )
         edge_attr = torch.cat((edge_attr, self_loop_attr), dim=0)
 
         # generate edge embeddings and propagate
-        edge_embeddings = self.edge_embedding1(edge_attr[:, 0]) + self.edge_embedding2(edge_attr[:, 1])
+        edge_embeddings = self.edge_embedding1(
+            edge_attr[:, 0]
+        ) + self.edge_embedding2(edge_attr[:, 1])
         return self.propagate(edge_index, x=x, edge_attr=edge_embeddings)
 
     def message(self, x_j, edge_attr):
@@ -162,16 +191,41 @@ class GCNConv(MessagePassing):
 
     @staticmethod
     def norm(edge_index, num_nodes, dtype):
+        """
+        Symmetric normalisation step of the adjacency matrix A:
+        .. math::
+            \\Tilde{A}=D^{-\frac{1}{2}}AD^{-\frac{1}{2}}
+        where D is the diagonal degree matrix.
+        Args:
+            edge_index: adjacency list
+            num_nodes: number of nodes
+            dtype: dtype of node features
 
-        # symmetrically normalise edge weights
-        edge_weight = torch.ones((edge_index.size(1),), dtype=dtype, device=edge_index.device)
+        Returns: symmetrically normalised edge weights
+
+        """
+
+        edge_weight = torch.ones(
+            (edge_index.size(1),), dtype=dtype, device=edge_index.device
+        )
         row, col = edge_index
         deg = scatter_add(edge_weight, row, dim=0, dim_size=num_nodes)
         deg_inv_sqrt = deg.pow(-0.5)
-        deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
+        deg_inv_sqrt[deg_inv_sqrt == float("inf")] = 0
         return deg_inv_sqrt[row] * edge_weight * deg_inv_sqrt[col]
 
     def forward(self, x, edge_index, edge_attr):
+        """
+        Message passing and aggregation function
+        of the adapted GCN convolutional layer.
+        Args:
+            x: node features
+            edge_index: adjacency list
+            edge_attr: edge features
+
+        Returns: transformed and aggregated node embeddings
+
+        """
 
         # add self loops to edge index
         edge_index, _ = add_self_loops(edge_index, num_nodes=x.size(0))
@@ -179,16 +233,20 @@ class GCNConv(MessagePassing):
         # add features corresponding to self-loop edges.
         self_loop_attr = torch.zeros(x.size(0), 2)
         self_loop_attr[:, 0] = self_loop_token
-        self_loop_attr = self_loop_attr.to(edge_attr.device).to(edge_attr.dtype)
+        self_loop_attr = self_loop_attr.to(edge_attr.device).to(
+            edge_attr.dtype
+        )
         edge_attr = torch.cat((edge_attr, self_loop_attr), dim=0)
 
         # generate edge embeddings and norm, and propagate
-        edge_embeddings = self.edge_embedding1(edge_attr[:, 0]) + self.edge_embedding2(edge_attr[:, 1])
+        edge_embeddings = self.edge_embedding1(
+            edge_attr[:, 0]
+        ) + self.edge_embedding2(edge_attr[:, 1])
         return self.propagate(
             edge_index,
             x=self.linear(x),
             edge_attr=edge_embeddings,
-            norm=self.norm(edge_index, x.size(0), x.dtype)
+            norm=self.norm(edge_index, x.size(0), x.dtype),
         )
 
     def message(self, x_j, edge_attr, norm):
@@ -200,12 +258,15 @@ class GNN(torch.nn.Module):
     Combine multiple GNN layers into a network.
     """
 
-    def __init__(
-            self,
-            num_layers=5,
-            embed_dim=300,
-            gnn_type='gin'
-    ):
+    def __init__(self, num_layers=5, embed_dim=300, gnn_type="gin"):
+        """
+        Compose convolution layers into GNN. Pretrained parameters
+        exist for a 5-layer network with 300 hidden units.
+        Args:
+            num_layers: number of convolution layers
+            embed_dim: dimension of node embeddings
+            gnn_type: type of convolutional layer to use
+        """
 
         self.num_layers = num_layers
         self.embed_dim = embed_dim
@@ -215,7 +276,9 @@ class GNN(torch.nn.Module):
 
         # initialise label embeddings
         self.x_embedding1 = torch.nn.Embedding(num_atom_type, self.embed_dim)
-        self.x_embedding2 = torch.nn.Embedding(num_chirality_tag, self.embed_dim)
+        self.x_embedding2 = torch.nn.Embedding(
+            num_chirality_tag, self.embed_dim
+        )
         torch.nn.init.xavier_uniform_(self.x_embedding1.weight.data)
         torch.nn.init.xavier_uniform_(self.x_embedding2.weight.data)
 
@@ -227,7 +290,7 @@ class GNN(torch.nn.Module):
             elif gnn_type == "gcn":
                 self.gnns.append(GCNConv(emb_dim=self.embed_dim))
             else:
-                raise NotImplementedError('Invalid GNN layer type.')
+                raise NotImplementedError("Invalid GNN layer type.")
 
         # initialise BatchNorm layers
         self.batch_norms = torch.nn.ModuleList()
@@ -235,20 +298,46 @@ class GNN(torch.nn.Module):
             self.batch_norms.append(torch.nn.BatchNorm1d(self.embed_dim))
 
     def load_pretrained(self, pretrain_type, device):
+        """
+        Checks if a pretrained parameter set for the specified
+        pretraining procedure exists and updates the
+        network's state dict if it does..
+        Args:
+            pretrain_type: type of pretraining procedure
+            device: device to which to load the saved state dict
+
+        Returns:
+
+        """
         pretrain_path = os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
-            'pretrained_params',
-            f'hu_iclr_2020_{self.gnn_type}_{pretrain_type}.pth'
+            "pretrained_params",
+            f"hu_iclr_2020_{self.gnn_type}_{pretrain_type}.pth",
         )
         if not os.path.isfile(pretrain_path):
             raise FileNotFoundError(
-                f'Specified GNN type ({self.gnn_type}) was not pretrained '
-                f'with requested approach ({pretrain_type}).')
+                f"Specified GNN type ({self.gnn_type}) was not pretrained "
+                f"with requested approach ({pretrain_type})."
+            )
         else:
-            pretrained_state_dict = torch.load(pretrain_path, map_location=device)
+            pretrained_state_dict = torch.load(
+                pretrain_path, map_location=device
+            )
             self.load_state_dict(pretrained_state_dict)
 
     def forward(self, x, edge_index, edge_attr):
+        """
+        Forward function of the GNN class that takes a PyTorch geometric
+        representation of a molecule or a batch of molecules
+        and generates the node embeddings for each atom.
+        Args:
+            x: node features
+            edge_index: adjacency list
+            edge_attr: edge features
+
+        Returns: tensor of num_nodes x embedding_dim embeddings
+
+        """
 
         # x[:, 0] corresponds to 'possible_atomic_num_list',
         # x[:, 1] corresponds to 'possible_chirality_list'
