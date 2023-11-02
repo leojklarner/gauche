9e476854fdd7bc501d4ce88720e618241be01f25
Leo Klarner <leo.klarner@bnc.ox.ac.uk>
2023-11-02 14:41:49 -0400

    Added more molprop dataloader tests.

--- tests/test_dataloaders/test_molprop_loader.py
+++ tests/test_dataloaders/test_molprop_loader.py
@@ -8,7 +8,6 @@ import pytest
 import os
 import itertools
 import numpy as np
-import networkx as nx
 from gauche.dataloader import MolPropLoader
 
 
@@ -72,8 +71,48 @@ def test_benchmark_loader(dataset, representation, kwargs):
     dataloader_csv.featurize(representation, **kwargs)
 
     # check if both dataloaders have the same features and labels
+    # to ensure that there is no stochasticity in the featurization
     # skip for molecular_graphs representation, since that might
     # require an isomorphism test
     if representation != "molecular_graphs":
         assert np.array_equal(dataloader.features, dataloader_csv.features)
     assert np.array_equal(dataloader.labels, dataloader_csv.labels)
+
+
+def test_invalid_data():
+    """
+    Test data loader for invalid input csv.
+    """
+
+    dataloader = MolPropLoader()
+    dataloader.read_csv(
+        path=os.path.join(os.getcwd(), "invalid_molprop_data.csv"),
+        smiles_column="SMILES",
+        labels_column="labels",
+    )
+    assert len(dataloader.features) == 2 and len(dataloader.labels) == 2
+
+
+@pytest.mark.parametrize(
+    "representation", ["XYZ", 2, True, None, ("ecfp_fingerprints", "fragments")]
+)
+def test_invalid_representation(representation):
+    """
+    Test behaviour of data loader for invalid representation choice.
+    """
+
+    dataloader = MolPropLoader()
+    dataloader.load_benchmark("ESOL")
+    with pytest.raises(Exception):
+        dataloader.featurize(representation)
+
+
+@pytest.mark.parametrize("benchmark", ["XYZ", 2, True, None, ("ESOL", "Photoswitch")])
+def test_invalid_benchmark(benchmark):
+    """
+    Test behaviour of data loader for invalid benchmark choice.
+    """
+
+    dataloader = MolPropLoader()
+    with pytest.raises(Exception):
+        dataloader.load_benchmark(benchmark)
